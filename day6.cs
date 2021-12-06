34509f5a6b9398f51513eb6e23e222d6739ef762
Fokko Veegens <fokkov@delta-n.nl>
2021-12-06 11:42:55 +0100

    List instead of array

--- day6.cs
+++ day6.cs
@@ -11,16 +11,14 @@ namespace AdventOfCode
         public static int Run(string path)
         {
             string input = common.GetInput(path);
-            int arrayIncreaseInterval = 1000000;
 
-            int[] fishes = input.Split(',').Select(int.Parse).ToArray();
-            int fishCount = fishes.Length;
+            List<int> fishes = input.Split(',').Select(int.Parse).ToList();
 
             for (int day = 1; day <= 80; day++)
             {
-                Console.WriteLine("{0} Day {1} (fish count: {2})", DateTime.Now.ToString("HH:mm:ss"), day, fishCount);
+                Console.WriteLine("{0} Day {1} (fish count: {2})", DateTime.Now.ToString("HH:mm:ss"), day, fishes.Count);
                 int fishesToAdd = 0;
-                for (int fish = 0; fish < fishCount; fish++)
+                for (int fish = 0; fish < fishes.Count; fish++)
                 {
                     // Produces new fish
                     if (fishes[fish] == 0)
@@ -38,15 +36,10 @@ namespace AdventOfCode
                 // Add fishes outside loop, otherwise days are already deducted on the same day as the fish was produced
                 for (int i = 0; i < fishesToAdd; i++)
                 {
-                    if (fishCount == fishes.Length)
-                    {
-                        Array.Resize(ref fishes, fishCount + arrayIncreaseInterval);
-                    }
-                    fishes[fishCount] = 8;
-                    fishCount++;
+                    fishes.Add(8);
                 }
             }
-            return fishCount;
+            return fishes.Count;
         }
     }
 }
